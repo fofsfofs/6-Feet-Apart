2c8d5ffadf0b0253e5a0e642b520a8e8eaa5f9b6
Sanjeev Sritharan <swaggermcpuff@gmail.com>
2020-05-16 18:16:40 -0400

    slightly better functioning chat room

--- public/client.js
+++ public/client.js
@@ -1,57 +1,9 @@
-const socket = new WebSocket("ws://192.168.0.22:3000");
-
-let ans = prompt("What is your name?");
-var vid = document.getElementById("vid");
-var videoID;
-
-socket.addEventListener("open", () => {
-  socket.send(ans);
+$(function () {
+  var socket = io();
+  $('form').submit(function(e) {
+    e.preventDefault(); // prevents page reloading
+    socket.emit('chat message', $('#m').val());
+    $('#m').val('');
+    return false;
+  });
 });
-
-socket.addEventListener("message", (event) => {
-  console.log(`Message from server: ${event.data}`);
-  if (event.data == "playing" && vid.paused == true) {
-    console.log("hi");
-    var playPromise = vid.play();
-
-    if (playPromise !== undefined) {
-      playPromise
-        .then((_) => {
-          // Automatic playback started!
-          // Show playing UI.
-        })
-        .catch((error) => {
-          // Auto-play was prevented
-          // Show paused UI.
-        });
-    }
-  } else {
-    vid = document.getElementById("vid");
-    videoID = event.data;
-    setTimeout(queue, 3000);
-  }
-});
-
-function sendURL() {
-  var link = document.getElementById("link").value;
-  socket.send(link);
-}
-
-function queue() {
-  vid.src = videoID + ".mp4";
-  vid.load();
-  vid.style.visibility = "visible";
-}
-
-function play() {
-  //   console.log("pressed");
-  socket.send("playing");
-  setInterval(isPlaying, 100);
-}
-
-function isPlaying() {
-  var vid = document.getElementById("vid");
-  if (vid.paused) {
-    socket.send("PAUSED");
-  }
-}
--- server.js
+++ server.js
@@ -1,38 +1,17 @@
-var express = require("express");
-var ws = require("ws");
-var app = express();
-const fs = require("fs");
-const ytdl = require("ytdl-core");
-const server = new ws.Server({ server: app.listen(3000) });
+var app = require('express')();
+var http = require('http').createServer(app);
+var io = require('socket.io')(http);
 
-app.use(express.static("public"));
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/public/index.html');
+});
 
-let queue = [];
-
-server.on("connection", function connection(ws, req) {
-  ws.on("message", function incoming(data) {
-    if (data.includes("youtube")) {
-      if (queue.length > 0) {
-        fs.unlinkSync("./public/" + queue[queue.length - 1] + ".mp4");
-      }
-
-      videoID = data.substring(data.indexOf("=") + 1, data.length);
-      queue.push(videoID);
-      server.clients.forEach((client) => {
-        client.send(videoID);
-      });
-      ytdl(data, { quality: "highest" }).pipe(
-        fs.createWriteStream("./public/" + queue[queue.length - 1] + ".mp4")
-      );
-    } else if (data.includes("PAUSED")) {
-      //   console.log(data);
-    } else if (data.includes("playing")) {
-      server.clients.forEach((client) => {
-        client.send(data);
-      });
-    } else {
-      console.log(data + " has CONNECTED!");
-    }
+io.on('connection', (socket) => {
+  socket.on('chat message', (msg) => {
+    io.emit('chat message', socket.id + ': ' + msg);
   });
-  //   console.log(req.ip);
+});
+
+http.listen(3000, () => {
+  console.log('listening on *:3000');
 });
