1c415533546126a9a2a706801f094650a7219092
Sanjeev Sritharan <swaggermcpuff@gmail.com>
2020-05-16 22:53:44 -0400

    functioning chat

--- server.js
+++ server.js
@@ -2,14 +2,24 @@ var app = require('express')();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
 
+var users = [];
+
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/public/index.html');
 });
 
 io.on('connection', (socket) => {
-  socket.on('chat message', (msg) => {
-    io.emit('chat message', socket.id + ': ' + msg);
+  socket.on('join', name => {
+    users[socket.id] = name;
+    io.emit("update", name + " has joined the server.")
+    io.emit("update-people", users);
   });
+  
+
+  socket.on('chat message', (msg) => {
+    io.emit('chat message', users[socket.id] + ': ' + msg);
+  });
+
 });
 
 http.listen(3000, () => {
